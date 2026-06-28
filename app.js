439d19ae6a9c431f5faa006852b1cd25832803fd
Manoj Tamang <tamangmanoj2053@gmail.com>
2026-06-28 13:01:05 +0545

    Initial project setup

---
+++ app.js
@@ -0,0 +1,14 @@
+// ===============================
+// SabdhaSathi
+// Main Application
+// ===============================
+
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const results = document.getElementById("results");
+
+function init() {
+    console.log("SabdhaSathi started 🚀");
+}
+
+init();
